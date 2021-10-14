bde019362cb04e3c50768f923c80428427a4824c
JustTory <johnnynathany@gmail.com>
2021-10-14 16:56:23 +0700

    productID in URL

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -12,8 +12,9 @@ import { Box } from '@mui/system';
 
 const ProductItem = ({ product, isSlider }) => {
     const formatedPrice = new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.price)
+    const productURL = '/product/' + encodeURIComponent(product.name).replace(/%20/g, '-') + `?i=${product.productID}`;
     return (
-        <Link to={`/product/${product.name}`} style={styles.link}>
+        <Link to={productURL} style={styles.link}>
             <Card sx={isSlider ? styles.cardSlider : styles.card}>
                 <Box sx={styles.box}>
                     <CardMedia
