2a302c6ac00d187a539592ab97a7e107806365cf
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-10-14 22:39:03 +0700

    complete address list + components

---
+++ src/components/CardAddresss/CardAddress.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+	Card,
+	CardContent,
+	Typography,
+	Button,
+	Divider,
+	CardActions,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const CardAddress = ({ address }) => {
+	return (
+		<Card variant="outlined" sx={{ minWidth: 275, my: 2 }}>
+			<CardContent >
+				<Typography variant="h6" component="div">
+					Name: {address.name} <br/>
+					Address: {address.address} <br/>
+					Phone: {address.phone}
+				</Typography>
+			</CardContent>
+		
+			<Divider sx={{ mx: 2 }}></Divider>
+
+			<CardActions sx={{ justifyContent: "flex-end", bgcolor: "#f5f5f5" }}>
+				<Button
+					variant="outlined"
+					size="small"
+					color="success"
+					startIcon={<DeleteIcon fontSize="small" />}>
+					Edit
+				</Button>
+				<Button
+					variant="outlined"
+					size="small"
+					color="error"
+					startIcon={<DeleteIcon fontSize="small" />}>
+					Delete
+				</Button>
+			</CardActions>
+		</Card>
+	);
+};
+export default CardAddress;
--- src/pages/AddressBook/AddressBook.js
+++ src/pages/AddressBook/AddressBook.js
@@ -1,11 +1,60 @@
-import React from 'react'
-
+import { React, useState } from "react";
+import {
+	FormControl,
+	InputLabel,
+	Input,
+	Box,
+	Container,
+	FormHelperText,
+	Card,
+	Typography,
+	CardContent,
+	Button,
+	CardActions,
+	Divider,
+} from "@mui/material";
+import CardAddress from "../../components/CardAddresss/CardAddress"
+// import axios from "axios";
 const AddressBook = () => {
-    return (
-        <div>
-            <h1>Address Book</h1>
-        </div>
-    )
-}
+	const [addressBook, setAddressBook] = useState([
+		{
+			deliveryID: 19,
+			address: "Huỳnh Văn Bánh, P.13, Phu Nhuan, Ho Chi Minh",
+			name: "Minh Đạo",
+			phone: "0785031700",
+			userID: 13,
+		},
+		{
+			deliveryID: 20,
+			address: "471 Tung Thien Vuong Street, 12, 8, HCM City",
+			name: "hehe",
+			phone: "0822453279",
+			userID: 13,
+		},
+	]);
+	//   axios
+	//     .get(
+	//       "http://technowdb.phuhanh.com.vn/api/deliAPi.php?command=getdelivery&userID=13"
+	//     )
+	//     .then((res) => {
+	//       setAddressBook(res.data)
+	//     });
+	return (
+		<Container>
+			<Box sx={{ textAlign: "center", py: 10 }}>
+				<Typography variant="h1" component="div">
+					Address Book
+				</Typography>
+			</Box>
+			{addressBook.map(address => (
+				<CardAddress address={address} key={address.deliveryID}/>
+			))}
+			<FormControl fullWidth="true">
+				<InputLabel htmlFor="my-input">Email address</InputLabel>
+				<Input id="my-input" aria-describedby="my-helper-text" />
+			</FormControl>
+		</Container>
+	);
+};
 
-export default AddressBook
+export default AddressBook;
