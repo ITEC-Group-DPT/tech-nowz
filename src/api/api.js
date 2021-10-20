eeca5679dbe30db07e09c3b39a66044e11be2940
JustTory <johnnynathany@gmail.com>
2021-10-20 20:51:19 +0700

    test api

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
-import { BASE_API_URL } from '../constant/string'
-
+import { BASE_API_URL, TEST_API_URL } from '../constant/string'
 
 const getProductCategory = (category, limit = 8) => {
-
     let command = "getProductCategory";
-
     return axios.get(BASE_API_URL + `productAPI.php?command=${command}&typeOfProduct=${category}&limit=${limit}`);
 }
 
 const getTopRating = (limit = 10) => {
-
     let command = "getTopRating";
-
     return axios.get(BASE_API_URL + `productAPI.php?command=${command}&limit=${limit}`);
 }
 
-export { getProductCategory, getTopRating };
+const getProduct = (productID) => {
+    let command = "getProduct";
+    return axios.get(TEST_API_URL + `productAPI.php?command=${command}&productID=${productID}`);
+}
+
+export { getProductCategory, getTopRating, getProduct };
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -3,15 +3,23 @@ import styles from './Product.styles'
 import { useParams, useLocation } from "react-router-dom";
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
-
+import { getProduct } from '../../api/api';
 const useQuery = () => {
     return new URLSearchParams(useLocation().search);
 }
 
 const Product = () => {
-    const { name } = useParams();
-    const query = useQuery();
+    const { name } = useParams()
+    const query = useQuery()
     const id = query.get("i")
+    getProduct(id).then(response => {
+        // if (response.data.success) {}
+        // else {}
+        if(response.status === 200) {
+            console.log(response.data);
+        }
+    })
+
     return (
         <Box sx={styles.wrapper}>
             <Typography component="div" sx={styles.typo}>Product Name: {name} </Typography>
