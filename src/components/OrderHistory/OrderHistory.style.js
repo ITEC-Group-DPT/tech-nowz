f24adec55d26ad29eaebe3a3f2b6f07d7ebfdf8a
chanhbeck <chanhbeck27@gmail.com>
2021-10-20 22:43:14 +0700

    heh

---
+++ src/components/OrderHistory/OrderHistory.style.js
@@ -0,0 +1,12 @@
+const styles = {
+    main: {
+        justifyContent:'center', 
+        alignItems:'center',
+    },
+    title: {
+        textAlign: "center",
+        fontSize: "2rem",
+    }
+};
+
+export default styles;
