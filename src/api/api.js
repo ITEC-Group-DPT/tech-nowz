f4f65c34cbc8cb0e45a7e67fce4f6545c7a424ed
JustTory <johnnynathany@gmail.com>
2021-10-23 21:05:06 +0700

    TEST_API_URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { BASE_API_URL } from '../constant/string'
+import { BASE_API_URL, TEST_API_URL } from '../constant/string'
 
 const getProductCategoryAPI = (category, limit = 8) => {
     let command = "getProductCategory";
-    return axios.get(BASE_API_URL + `productAPI.php?command=${command}&typeOfProduct=${category}&limit=${limit}`);
+    return axios.get(TEST_API_URL + `productAPI.php?command=${command}&typeOfProduct=${category}&limit=${limit}`);
 }
 
 const getTopRatingAPI = (limit = 10) => {
     let command = "getTopRating";
-    return axios.get(BASE_API_URL + `productAPI.php?command=${command}&limit=${limit}`);
+    return axios.get(TEST_API_URL + `productAPI.php?command=${command}&limit=${limit}`);
 }
 
 export { getProductCategoryAPI, getTopRatingAPI };
