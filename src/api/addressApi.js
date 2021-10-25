7ec2d0ac59c4cc3d5af476370caa331bdf48c1f9
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-10-25 10:48:37 +0700

    update axios for addressbook

--- src/api/addressApi.js
+++ src/api/addressApi.js
@@ -1,35 +1,36 @@
-import axios from 'axios';
-import { BASE_API_URL, TEST_API_URL } from '../constant/string'
+import axios from "./axios";
+import { BASE_API_URL, TEST_API_URL } from "../constant/string";
 
 const getAddressBook = () => {
-    let command = "getDelivery";
-    let header = {userid: 17}
-    return axios.get(TEST_API_URL + `deliAPI.php?command=${command}`,{headers: header});
-}
-const createAddressBook = (name,address,phone) => {
-    let header = {userid: 17}
-    let data = new FormData();
-    data.append("command", "create");
-    data.append("name", name);
-    data.append("address", address);
-    data.append("phone", phone);
-    return axios.post(TEST_API_URL + `deliAPI.php`,data, {headers: header});
-}
-const deleteAddressBook = (id)=>{
-    let header = {userid: 17}
-    let data = new FormData();
-    data.append("command", "delete");
-    data.append("deliID", id);
-    return axios.post(TEST_API_URL + `deliAPI.php`,data, {headers: header});
-}
-const editAddressBook =(id,name,address,phone) => {
-    let header = {userid: 17}
-    let data = new FormData();
-    data.append("command", "update");
-    data.append("deliID",id)
-    data.append("name", name);
-    data.append("address", address);
-    data.append("phone", phone);
-    return axios.post(TEST_API_URL + `deliAPI.php`,data, {headers: header});
-}
-export { getAddressBook,createAddressBook,deleteAddressBook,editAddressBook};
+	let command = "getDelivery";
+	return axios.get(TEST_API_URL + `deliAPI.php?command=${command}`);
+};
+const createAddressBook = (name, address, phone) => {
+	let data = new FormData();
+	data.append("command", "create");
+	data.append("name", name);
+	data.append("address", address);
+	data.append("phone", phone);
+	return axios.post(TEST_API_URL + `deliAPI.php`, data);
+};
+const deleteAddressBook = (id) => {
+	let data = new FormData();
+	data.append("command", "delete");
+	data.append("deliID", id);
+	return axios.post(TEST_API_URL + `deliAPI.php`, data);
+};
+const editAddressBook = (id, name, address, phone) => {
+	let data = new FormData();
+	data.append("command", "update");
+	data.append("deliID", id);
+	data.append("name", name);
+	data.append("address", address);
+	data.append("phone", phone);
+	return axios.post(TEST_API_URL + `deliAPI.php`, data);
+};
+export {
+	getAddressBook,
+	createAddressBook,
+	deleteAddressBook,
+	editAddressBook,
+};
