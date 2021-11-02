f1dd9c3d7c8f91c1532ad2839f36b9f12527ef9a
kurozemi <tranngminhdao@gmail.com>
2021-11-02 21:44:21 +0700

    fix base api for build

--- src/constant/string.js
+++ src/constant/string.js
@@ -3,6 +3,6 @@ const string = {
 };
 
 const BASE_API_URL = 'http://technowdb.phuhanh.com.vn/api/';
-const TEST_API_URL = 'http://localhost:8080/Technowz_Backend/api/';
+const TEST_API_URL = 'https://technowbe.up.railway.app/api/';
 
 export { string, BASE_API_URL, TEST_API_URL };
