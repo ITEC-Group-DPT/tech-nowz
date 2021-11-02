ed0d97b6d0c1e00ac4a71aac3a58c94fa4f9d717
kurozemi <tranngminhdao@gmail.com>
2021-11-02 17:11:45 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,18 +8,17 @@ const instance = axios.create({
 const object = sessionStorage.getItem('userInfo');
 if (object) {
 	const { userID } = JSON.parse(object);
-	console.log('userID: ', userID);
 	// Alter defaults after instance has been created
-	instance.defaults.headers.common['userid'] = userID;
+	instance.defaults.headers.common['Userid'] = userID;
 }
 
 instance.interceptors.request.use(
 	(config) => {
-		if (!config.headers.userid) {
+		if (!config.headers.Userid) {
 			console.log('run interceptor');
 			const token = JSON.parse(sessionStorage.getItem('userInfo'));
 			if (token) {
-				config.headers.userid = token.userID;
+				config.headers.Userid = token.userID;
 			}
 		}
 		return config;
