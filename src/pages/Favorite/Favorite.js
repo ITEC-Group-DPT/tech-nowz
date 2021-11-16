501c04f0bdda380211e3270fdd24ae24bbb3699a
JustTory <johnnynathany@gmail.com>
2021-11-16 10:21:19 +0700

    favorite isLoading UI

--- src/pages/Favorite/Favorite.js
+++ src/pages/Favorite/Favorite.js
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import HorizontalProduct from '../../components/HorizontalProduct/HorizontalProduct'
+import HorizontalProductSkeleton from '../../components/HorizontalProductSkeleton/HorizontalProductSkeleton'
 import styles from './Favorite.styles'
 import { Container, Typography, Collapse } from '@mui/material'
 import { getFavoriteListApi, changeFavoriteApi } from '../../api/favoriteApi'
 import { Box } from '@mui/system'
-import { TransitionGroup } from 'react-transition-group';
+import { TransitionGroup } from 'react-transition-group'
 
 const Favorite = () => {
-    const [list, setFavoriteList] = useState([]);
+    const [favoriteList, setFavoriteList] = useState({ "isLoading": true })
 
     useEffect(() => {
         getFavoriteListApi().then(response => {
             if (response.data.success == true) {
-                setFavoriteList(response.data.data);
-                console.log("Fav list: ", response.data.data);
+                setFavoriteList({ "isLoading": false, "data": response.data.data })
+                console.log("Fav list: ", response.data.data)
             }
         })
     }, [])
 
     const onDelete = (productID) => {
         changeFavoriteApi(productID).then(response => {
-            console.log(response);
+            console.log(response)
             if (response.data.success == true) {
                 if (response.data.data.isLike == false) {
-                    let newList = list.filter((product) => product.productID !== productID);
-                    console.log(newList);
-                    setFavoriteList(newList);
+                    let newList = favoriteList.data.filter((product) => product.productID !== productID)
+                    console.log(newList)
+                    setFavoriteList({ ...favoriteList, "data": newList })
                 }
             }
         })
@@ -35,22 +36,34 @@ const Favorite = () => {
         <Box sx={styles.box}>
             <Typography sx={styles.sliderTitle}>Favorites</Typography>
             <Container maxWidth="md">
-                <TransitionGroup>
-                    {list.map(product =>
-                        <Collapse key={product.productID}>
-                            <HorizontalProduct
-                                product={product}
-                                key={product.productID}
-                                canDelete={true}
-                                ratingSize={"20px"}
-                                onPressDelete={(e) => {
-                                    e.preventDefault()
-                                    onDelete(product.productID)
-                                }}
-                            />
-                        </Collapse>
-                    )}
-                </TransitionGroup>
+                {favoriteList.isLoading ? (
+                    <TransitionGroup>
+                        {Array(1).fill().map(() => (
+                            <Collapse >
+                                <HorizontalProductSkeleton />
+                            </Collapse>
+                        ))}
+                    </TransitionGroup>
+                ) : (
+                    <TransitionGroup>
+                        {favoriteList.data.map(product =>
+                            <Collapse key={product.productID}>
+                                <HorizontalProduct
+                                    product={product}
+                                    key={product.productID}
+                                    canDelete={true}
+                                    ratingSize={"20px"}
+                                    onPressDelete={(e) => {
+                                        e.preventDefault()
+                                        onDelete(product.productID)
+                                    }}
+                                />
+                            </Collapse>
+                        )}
+                    </TransitionGroup>
+                )}
+
+
             </Container>
         </Box>
     )
