7f9753a27f0d8bb7fc7f9c6ae08cca20bad1d323
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2022-01-09 22:07:53 +0700

    again

--- src/constant/string.js
+++ src/constant/string.js
@@ -4,10 +4,8 @@ const string = {
 
 const AUTH_KEY = "CDLPT2001";
 const BASE_API_URL = 'http://technowdb.phuhanh.com.vn/api/';
-<<<<<<< Updated upstream
-const TEST_API_URL = 'https://technowbe.up.railway.app/api/';
-=======
+
 const TEST_API_URL = 'http://technow.phuhanh.com.vn/api/';
->>>>>>> Stashed changes
+
 
 export { string, BASE_API_URL, TEST_API_URL ,AUTH_KEY};
